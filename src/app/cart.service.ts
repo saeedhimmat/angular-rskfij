84b71b13c3b5d1638190405bb23062fa5f09585f
saeedhimmat <inf3271@hs-worms.de>
2022-06-07 18:04:40 +0200

    2 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
+import { Product } from './products';
+/* . . . */
 export class CartService {
+  items: Product[] = [];
+/* . . . */
 
-  constructor() { }
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+/* . . . */
 }
